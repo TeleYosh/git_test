a36dd364ba88fbfda76da2122a8a5948e06e6a0d
Yosh <ilyass.mejdoub@pm.me>
2025-11-06 15:36:03 +0100

    landing page for ml interface with uploading function

--- dom/script.js
+++ dom/script.js
@@ -2,8 +2,21 @@
 
 function addItem(e, item) {
     const ul = document.querySelector('ul');
-    ul.innerHTML = `<li>${item}</li>`;
+    ul.innerHTML += `
+    <div class='item ${item}'>
+        <li>${item}</li>
+        <button class='delete'>delete</button>
+    </div>
+    `;
+    const newDeleteBtn = ul.querySelector(`.item.${item} .delete`);
+    newDeleteBtn.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+    });
+}
 
+function deleteItem(e, item) {
+    const div = document.querySelector(`.${item}, .item`);
+    div.innerHTML = '';
 }
 
 const form = document.querySelector('form');
@@ -11,4 +24,13 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const textValue = form.elements.text.value;
     addItem(e, textValue);
+    form.elements.text.value = '';
+    // const dlt_btn = document.querySelector('.delete');
+    // dlt_btn.addEventListener('click', (e) => {
+    //     const item = dlt_btn.parentElement.className;
+    //     console.log(`item name: ${item.split(' ')[1]}`);
+    //     deleteItem(e, item);
+    // });
 });
+
+
