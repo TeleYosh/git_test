5d7c627cc413fcaadc5da93fdd893c1f028c7e99
Yosh <ilyass.mejdoub@pm.me>
2025-11-07 16:52:34 +0100

    function to generate grid and button to reset it functional

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,27 +1,50 @@
 const grid = document.querySelector('.container');
 const [nRows, nCols] = [16, 16];
-const gridLenght = '30px';
+// const gridLenght = '30px';
 
-for (let i=1; i<nRows+1; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.setAttribute('id', `row-${i}`);
-    rowDiv.setAttribute('style', 
-        `
-        display:flex;
-        height: ${gridLenght};
-        outline: 2px solid black;
-        `);
-    for (let j=1; j<nCols+1; j++) {
-        const gridSquareDiv = document.createElement('div');
-        gridSquareDiv.setAttribute('id', `col-${j}`);
-        gridSquareDiv.setAttribute('style', 
-            `  
-            width: ${gridLenght};
-            outline: 2px solid black;
+function generateGrid(nRows, nCols) {
+    for (let i=1; i<nRows+1; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.setAttribute('id', `row-${i}`);
+        rowDiv.setAttribute('class', 'row');
+        rowDiv.setAttribute('style', 
             `
-        )
-        rowDiv.appendChild(gridSquareDiv);
-    }   
-    grid.appendChild(rowDiv);
+            display:flex;
+            flex: 1;
+            outline: 0.5px solid black;
+            `);
+        for (let j=1; j<nCols+1; j++) {
+            const gridSquareDiv = document.createElement('div');
+            gridSquareDiv.setAttribute('id', `col-${j}`);
+            gridSquareDiv.setAttribute('class', 'gridSquare');
+            gridSquareDiv.setAttribute('style', 
+                `  
+                flex:1;
+                outline: 0.5px solid black;
+                `
+            )
+            rowDiv.appendChild(gridSquareDiv);
+        }   
+        grid.appendChild(rowDiv);
 
+    }
 }
+
+function wipeGrid(gridSquares) {
+    gridSquares.forEach((square) => {
+        square.style.backgroundColor = '';
+    })
+}
+
+generateGrid(nRows, nCols);
+
+const gridSquares = document.querySelectorAll('.gridSquare');
+gridSquares.forEach((square) => {
+    square.addEventListener('mouseover', (e) => {
+            square.style.backgroundColor = 'rgba(0, 0, 0, 0.35)';
+        }
+        )
+    })
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', (e) => wipeGrid(gridSquares))
